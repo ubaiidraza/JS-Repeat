320254d272c0180d7e20f37cdd1d3216f95a911e
ubaiidraza <154611273+ubaiidraza@users.noreply.github.com>
2024-12-01 19:33:48 +0500

    If else

---
+++ app.js
@@ -0,0 +1,31 @@
+
+ 
+//  var num=100;
+var num = + prompt ();
+if(num >= 1 && num <= 29 ){
+    console.log('riksha chalaly bhai');
+}
+else if(num >= 30 && num <= 39 ){
+    console.log('bchat ho gai');
+}
+else if(num >= 40 && num <= 49 ){
+    console.log('D grade bana ha');
+}
+else if(num >= 50 && num <= 59 ){
+    console.log('C grade bana ha');
+}  
+else if(num >= 60 && num <= 69 ){
+    console.log('B grade bana ha');
+}
+else if(num >= 70 && num <= 79 ){
+    console.log('A grade bana ha');
+}
+else if(num >= 80 && num <= 89 ){
+    console.log('A+ grade bana ha');
+}
+else if(num >= 90 && num <= 100 ){
+    console.log('A+1 grade bana ha');
+}
+
+
+
