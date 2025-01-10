22871223a1122a3b225c8a478edba7d8a340eacb
Ubaid Raza <154611273+ubaiidraza@users.noreply.github.com>
2025-01-10 15:43:17 +0500

    comment

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 
  
-//  var num=100;
-var num = + prompt ();
-if(num >= 1 && num <= 29 ){
-    console.log('riksha chalaly bhai');
-}
-else if(num >= 30 && num <= 39 ){
-    console.log('bachat ho gai warna ricshaw diladyty abbu ');
-}
-else if(num >= 40 && num <= 49 ){
-    console.log('paas hogye ');
-}
-else if(num >= 50 && num <= 59 ){
-    console.log('C grade bana ha');
-}  
-else if(num >= 60 && num <= 69 ){
-    console.log('B grade bana ha');
-}
-else if(num >= 70 && num <= 79 ){
-    console.log('A grade bana ha');
-}
-else if(num >= 80 && num <= 89 ){
-    console.log('A+ grade bana ha');
-}
-else if(num >= 90 && num <= 100 ){
-    console.log('A+1 grade bana ha');
-}
+// //  var num=100;
+// var num = + prompt ();
+// if(num >= 1 && num <= 29 ){
+//     console.log('riksha chalaly bhai');
+// }
+// else if(num >= 30 && num <= 39 ){
+//     console.log('bachat ho gai warna ricshaw diladyty abbu ');
+// }
+// else if(num >= 40 && num <= 49 ){
+//     console.log('paas hogye ');
+// }
+// else if(num >= 50 && num <= 59 ){
+//     console.log('C grade bana ha');
+// }  
+// else if(num >= 60 && num <= 69 ){
+//     console.log('B grade bana ha');
+// }
+// else if(num >= 70 && num <= 79 ){
+//     console.log('A grade bana ha');
+// }
+// else if(num >= 80 && num <= 89 ){
+//     console.log('A+ grade bana ha');
+// }
+// else if(num >= 90 && num <= 100 ){
+//     console.log('A+1 grade bana ha');
+// }
 
 
 
